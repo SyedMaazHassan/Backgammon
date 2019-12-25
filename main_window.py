69daac745aa0b8794eb5b898e4bf030dfe4cb472
Syed Maaz Hassan <hafizmaazhassan33@gmail.com>
2019-12-25 16:18:09 +0500

    Add files via upload

--- main_window.py
+++ main_window.py
@@ -3,7 +3,8 @@ import random
 import math
 import time
 from threading import Timer
-
+import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (150,30)
 #initializing pygame
 pg.init()
 size = (900, 900)  #a tuple of size (width, height)
@@ -114,19 +115,67 @@ def stack_column_position_bottom(x):
 def dice_value():
     value_1 = random.randint(1, 6)
     value_2 = random.randint(1, 6)
-    dice1.my_dice = pg.image.load(L[value_1-1])
-    dice2.my_dice = pg.image.load(L[value_2-1])
-    print(value_1, value_2)
-    return (value_1, value_2)
+    dice1.my_dice = pg.image.load(you_dice[value_1-1])
+    dice2.my_dice = pg.image.load(you_dice[value_2-1])
+    write_in_file("{} {}".format(value_1, value_2), "txt/dice_saving.txt")
+
+def cpu_dice_value():
+    value_1 = random.randint(1, 6)
+    value_2 = random.randint(1, 6)
+    dice1_cpu.my_dice = pg.image.load(cpu_dice_list[value_1-1])
+    dice2_cpu.my_dice = pg.image.load(cpu_dice_list[value_2-1])
+    final = "{} {}".format(value_1, value_2)
+    return final
+
+def write_in_file(content, file_name):
+    file = open(file_name, "w")
+    file.write(content)
+    file.close()
+
+def get_from_file(file_name = "txt/dice_saving.txt"):
+    file = open(file_name, "r")
+    a = file.read()[0]
+    b = file.read()[-1]
+    file.close()
+    return (int(a), int(b))
+
+#highlighting the keys
+def light_white_keys():
+    for i in all_stack_list:
+        light_piece = i.elements[-1]
+        if light_piece and light_piece.id == "white":
+            light_piece.image = pg.image.load("img/white_highlight.png")
+            white_light_pieces.append(light_piece)
+
+def light_black_keys():
+    for i in all_stack_list:
+        light_piece = i.elements[-1]
+        if light_piece and light_piece.id == "black":
+            light_piece.image = pg.image.load("img/black_highlight.png")
+            black_light_pieces.append(light_piece)
 
 #dice tolling list
-L = ["img/you_dice_1.png",
+you_dice = [
+     "img/you_dice_1.png",
      "img/you_dice_2.png",
      "img/you_dice_3.png",
      "img/you_dice_4.png",
      "img/you_dice_5.png",
      "img/you_dice_6.png"
      ]
+
+cpu_dice_list = [
+     "img/cpu_dice_1.png",
+     "img/cpu_dice_2.png",
+     "img/cpu_dice_3.png",
+     "img/cpu_dice_4.png",
+     "img/cpu_dice_5.png",
+     "img/cpu_dice_6.png"
+     ]
+
+white_light_pieces = [ ]
+
+black_light_pieces = [ ]
 
 L_e = 0
 
@@ -144,7 +193,7 @@ class white_piece:
         self.co_ordinate = co_ordinates
         self.X = self.co_ordinate[0]
         self.Y = self.co_ordinate[1]
-        self.id = "black"
+        self.id = "white"
 
 
     def move(self, new_x, new_y):
@@ -154,8 +203,8 @@ class white_piece:
 
 #for column stack class
 class column_stack:
-    def __init__(self, location):
-        self.elements = []
+    def __init__(self, location, *initial_pieces):
+        self.elements = list(initial_pieces)
         function = None
         if location < 13:
             function = stack_column_position_top(location)
@@ -163,7 +212,17 @@ class column_stack:
             function = stack_column_position_bottom(location)
         self.visible = pg.draw.polygon(screen, (0, 225, 0), function, 2)
 
+    # def highlight_destination(self):
+
+
+
 #dice
+class cpu_dice:
+    def __init__(self, pic):
+        self.my_dice = pg.image.load(pic)
+
+
+blank_cpu = "img/blank_cpu.png"
 blank = "img/blank.png"
 class player_dice:
     def __init__(self, pic):
@@ -171,6 +230,9 @@ class player_dice:
 
 dice1 = player_dice(blank)
 dice2 = player_dice(blank)
+
+dice1_cpu = cpu_dice(blank_cpu)
+dice2_cpu = cpu_dice(blank_cpu)
 
 #black pieces
 
@@ -222,9 +284,50 @@ active_dice_button = pg.image.load("img/active_dice_button.png")
 #blank dice
 dice = pg.image.load("img/blank.png")
 
+#checking highlights
+# white_piece14.image = pg.image.load("img/white_highlight.png")
+# black_piece5.image = pg.image.load("img/black_highlight.png")
 
+
+#stack objects
+
+stack1 = column_stack(1, black_piece14, black_piece15)
+stack2 = column_stack(2, None)
+stack3 = column_stack(3, None)
+stack4 = column_stack(4, None)
+stack5 = column_stack(5, None)
+stack6 = column_stack(6, white_piece1, white_piece2, white_piece3, white_piece4, white_piece5)
+stack7 = column_stack(7, None)
+stack8 = column_stack(8, white_piece6, white_piece7, white_piece8)
+stack9 = column_stack(9, None)
+stack10 = column_stack(10, None)
+stack11 = column_stack(11, None)
+stack12 = column_stack(12, black_piece9, black_piece10, black_piece11, black_piece12, black_piece13)
+stack13 = column_stack(13, white_piece13, white_piece12, white_piece11, white_piece10, white_piece9)
+stack14 = column_stack(14, None)
+stack15 = column_stack(15, None)
+stack16 = column_stack(16, None)
+stack17 = column_stack(17, black_piece8, black_piece7, black_piece6)
+stack18 = column_stack(18, None)
+stack19 = column_stack(19, black_piece5, black_piece4, black_piece3, black_piece2, black_piece1)
+stack20 = column_stack(20, None)
+stack21 = column_stack(21, None)
+stack22 = column_stack(22, None)
+stack23 = column_stack(23, None)
+stack24 = column_stack(24, white_piece15, white_piece14)
+
+all_stack_list = [
+    stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9, stack10, stack11, stack12,
+    stack13, stack14, stack15, stack16, stack17, stack18, stack19, stack20, stack21, stack22, stack23, stack24
+]
+
+you_dice_rolled = False
+temppp = 0
+temp = 0
+turn = "cpu"
 speed = 2.1
 running = True
+
 
 
 while running:
@@ -240,6 +343,12 @@ while running:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_RIGHT:
                 white_piece1.move(1,1)
+
+        if event.type == pg.MOUSEBUTTONUP and turn == "you" and 840 <= mouse[0] <= 885 and 440 <= mouse[1] <= 560:
+            if event.button == 1:
+                light_white_keys()
+                you_dice_rolled = True
+
 
 
 
@@ -275,15 +384,40 @@ while running:
     screen.blit(white_piece14.image, white_piece14.co_ordinate)
     screen.blit(white_piece15.image, white_piece15.co_ordinate)
 
-    if 840 <= mouse[0] <= 885 and 440 <= mouse[1] <= 560:
-        screen.blit(active_dice_button, (840, 440))
-        if click[0] == 1:
-            dice_value()
-    else:
-        screen.blit(inactive_dice_button, (840, 440))
+    if turn == "you" and you_dice_rolled == False:
+        if 840 <= mouse[0] <= 885 and 440 <= mouse[1] <= 560:
+            screen.blit(active_dice_button, (840, 440))
+            if click[0] == 1:
+                dice_value()
+        else:
+            screen.blit(inactive_dice_button, (840, 440))
+    elif turn == "cpu":
+        if temp == 0:
+            a = cpu_dice_value()
+            if temppp != 15:
+                temppp +=1
+            else:
+                write_in_file(a, "txt/cpu_dice_saving.txt")
+                temp = 1
+                light_black_keys()
+
+
+
+
+
+
+
+
+
+
+
 
     screen.blit(dice1.my_dice, (2, 650))
     screen.blit(dice2.my_dice, (2, 720))
+
+    screen.blit(dice1_cpu.my_dice, (2, 210))
+    screen.blit(dice2_cpu.my_dice, (2, 285))
+
 
 
     # for i in range(1, 25):
